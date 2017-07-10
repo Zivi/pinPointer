94713c443345e5434d81e31dd662f716c15a7ce4
Zivi Weinstock <zivi.weinstock@gmail.com>
2017-07-10 13:48:19 -0700

    Info window show on marker click.

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { Map, Marker, GoogleApiWrapper } from  'google-maps-react';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from  'google-maps-react';
 
 class Container extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      activeMarker: null,
+      showInfoWindow: false,
+    };
+  }
+
+
+  onMarkerClick(props, marker) {
+    debugger;
+    this.setState({
+      activeMarker: marker,
+      showInfoWindow: true,
+    });
+  }
+
   render() {
     const style = {
       width: '100px',
@@ -9,16 +27,30 @@ class Container extends React.Component {
     };
 
     const { marker } = this.props;
-
     return (
       <div style={style}>
         <Map google={this.props.google} center={this.props.center}>
-          {marker &&
+          {marker ?
             <Marker
               position={{ lat: marker.lat, lng: marker.lng}}
               name={marker.locationName}
+              onClick={this.onMarkerClick.bind(this)}
             />
+            /* Fix for issue in google maps react library where page won't load
+            if child is undefined
+            this be removed once this PR is merged: https://github.com/fullstackreact/google-maps-react/pull/66/files
+            https://github.com/fullstackreact/google-maps-react/blob/master/src/index.js#L222 */
+            : <div></div>
           }
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showInfoWindow}
+          >
+            <div>
+              <h1>{marker && marker.locationName}</h1>
+            </div>
+          </InfoWindow>
+
         </Map>
       </div>
     );
